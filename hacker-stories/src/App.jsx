6a6c15037afc71f9a62c28660b7c77a85d134511
Joey <josephrobertlopez@gmail.com>
2023-02-03 14:48:38 -0600

    React jsx

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+function getTitle(title){
+  return title;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id = "search" type="text"/>
+    </div>
+  )
+}
+export default App;
