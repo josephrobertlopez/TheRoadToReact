ace1f8bc4e4101e9a008da9b2b4a2f8133e338d7
Joey <josephrobertlopez@gmail.com>
2023-02-04 19:44:19 -0600

    React component Declaration

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -18,36 +18,33 @@ const list = [
   },
 ]
 
-function App() {
-  return (
+const App = () => (
   <div>
-  <h1>My Hacker Stories</h1>
-  {/* create an instance of List component */}
-  <List />
-  {/* create another instance of List component */}
-  <List />
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
   </div>
+);
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+const List = () => (
+  <ul>
+    {
+      list.map((item) => (
+          <li key = {item.objectID}>
+            <span>
+              <a href = {item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+    ))}
+  </ul>
   );
-}
-
-function List() {
-  return (
-    <ul>
-      {
-        list.map(function(item){
-          return (
-            <li key = {item.objectID}>
-              <span>
-                <a href = {item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })
-      }
-    </ul>
-  )
-}
 export default App;
