278bd8ffba63c55e8807e9bdf0f353b874b2f38e
Joey <josephrobertlopez@gmail.com>
2023-02-05 09:31:12 -0600

    React State

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 
 const App = () => {
+  console.log('App renders');
   const stories = [
     {
       title: 'React',
@@ -30,35 +31,44 @@ const App = () => {
   );
 };
 const Search = () => {
+  console.log('Search renders');
+  const [searchTerm, setSearchTerm] = React.useState('');
   const handleChange = (event) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   }
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange}/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 };
-const List = (props) => (
-  <ul>
-    {
-      props.list.map((item) => (
-        <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
-const Item = (props) => (
-  <li>
-    <span>
-      <a href = {props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
-)
+const List = (props) => {
+  console.log('List renders');
+  return(
+    <ul>
+      {
+        props.list.map((item) => (
+          <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+    );
+};
+const Item = (props) => {
+  console.log('Item renders');
+  return (  
+    <li>
+      <span>
+        <a href = {props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 export default App;
