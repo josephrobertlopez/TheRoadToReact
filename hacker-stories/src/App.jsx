268a1aa358dbe47305c1cf79668bdcd5a7489970
josephrobertlopez <josephrobertlopez@gmail.com>
2023-02-14 10:36:49 -0600

    Imperative React

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -57,18 +57,32 @@ const App = () => {
   </div>
   );
 };
-const InputWithLabel = ({id, value, type='text', onInputChange, children}) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input 
-      id={id}
-      type={type}
-      value={value}
-      onChange={onInputChange}
-    />
-  </>
-);
+const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, children}) => {
+  // A
+  const inputRef = React.useRef();
+  // C
+  React.useEffect(() => {
+    if(isFocused && inputRef.current){
+      // D
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      {/* B */}
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  )
+
+} 
 const List = ({list}) => (
   <ul>
     {
